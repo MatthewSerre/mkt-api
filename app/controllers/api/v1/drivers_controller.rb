a79e0938e4acf3440b1b6c99995ba7e9c0b6bcc2
Matthew Serre <matthew_serre+github@outlook.com>
2021-05-16 17:25:52 -0400

    Fix favorite_courses endpoint

--- app/controllers/api/v1/drivers_controller.rb
+++ app/controllers/api/v1/drivers_controller.rb
@@ -11,7 +11,22 @@ class Api::V1::DriversController < ApplicationController
     end
 
     def favorite_courses
-        connections = CourseDriverConnection.where(driver_id: Driver.find_by(name: params[:driver_name].titleize), is_favorite: true)
+        driver_id = Driver.find_by(name: params[:driver_name].titleize)
+        level = params[:level].to_i
+        case level
+        when 6..7
+            level6 = CourseDriverConnection.where(driver_id: driver_id, is_favorite_at_level_6: true)
+            level3 = CourseDriverConnection.where(driver_id: driver_id, is_favorite_at_level_3: true)
+            base = CourseDriverConnection.where(driver_id: driver_id, is_favorite: true)
+            connections = level6 + level3 + base
+            connections.sort_by {|obj| obj.id}
+        when 3..5
+            level3 = CourseDriverConnection.where(driver_id: driver_id, is_favorite_at_level_3: true)
+            base = CourseDriverConnection.where(driver_id: driver_id, is_favorite: true)
+            connections = level3 + base
+        else
+            connections = CourseDriverConnection.where(driver_id: driver_id, is_favorite: true)
+        end
         favorite_courses = []
         connections.each do |connection|
             course = Course.find(connection.course.id)
@@ -19,7 +34,7 @@ class Api::V1::DriversController < ApplicationController
         end
         render json: favorite_courses.to_json
     end
-    
+
     def high_end
         drivers = Driver.where(rarity: "High-End")
         render json: drivers.to_json
@@ -41,7 +56,6 @@ class Api::V1::DriversController < ApplicationController
     end
 
     def name_contains
-        binding.pry
         drivers = Driver.where("name LIKE ?", "%#{params[:driver_name].titleize}%")
         render json: drivers.to_json
     end
